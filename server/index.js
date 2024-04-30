5b00eeda17a70c16d58b7509adef1ffedbeb663c
Sajjad1996 <sajjad75926@gmail.com>
2024-04-30 17:31:14 +0330

    Import schema as .graphql and export resolvers.

--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
+import {loadSchemaSync} from '@graphql-tools/load';
+import {GraphQLFileLoader} from '@graphql-tools/graphql-file-loader';
 import {ApolloServer} from '@apollo/server';
 import {startStandaloneServer} from '@apollo/server/standalone';
+import {resolvers} from './graphql/queries.js';
+
+const schema = loadSchemaSync(
+    './graphql/schema.graphql',
+    {loaders: [new GraphQLFileLoader()]}
+);
 
 const server = new ApolloServer({
-    typeDefs: `#graphql
-    type Query {
-        hello: String
-    }`,
-    resolvers: {
-        Query: {
-            hello: () => 'Hello, World!',
-        },
-    }
+    typeDefs: schema,
+    resolvers: resolvers
 });
 
-startStandaloneServer(server, { listen: { port: 4000 }, })
-    .then(({ url }) => console.log(`Running a GraphQL API server at ${url}`));
+startStandaloneServer(server, {listen: {port: 4000},})
+    .then(({url}) => console.log(`Running a GraphQL API server at ${url}`));
